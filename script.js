b2a929209abe7dbc5505bdd6519c7b0f1ba02728
Francesca Salvini <franci98sp@gmail.com>
2021-03-23 16:46:20 +0100

    sistemati i commenti nel js

--- script.js
+++ script.js
@@ -8,20 +8,21 @@
 //Stampa i numeri da 1 a 100 
 for( var i = 1 ; i <= 100 ; i++) {
     
-    //per i numeri multipli di 3 stampa "Fizz"
+    //per i numeri multipli di 3 e 5 stampa "FizzBuzz"
     if((i % 3 == 0) && (i % 5 == 0) ) {
 
         console.log("FizzBuzz");
         
-    }else if(i % 3 == 0) {
+        
+    }else if(i % 3 == 0) { //per i numeri multipli di 3 stampa "Fizz"
         
         console.log("Fizz"); 
 
-    } else if(i % 5 == 0) {
+    } else if(i % 5 == 0) { //per i numeri multipli di 5 stampa "Buzz"
 
         console.log("Buzz");
 
-    }else {
+    }else { //per tutti gli altri numeri (non divisibili per 3, 5, 3 e 5) scrivi il numero
 
         console.log(i);
     }
