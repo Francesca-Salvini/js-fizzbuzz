83e44198d95404785ac57a91d0ea4036fba7567e
Francesca Salvini <franci98sp@gmail.com>
2021-03-23 15:49:58 +0100

    compleato esercizio

--- script.js
+++ script.js
@@ -3,3 +3,29 @@
 //Per i multipli di 5 stampi Buzz
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
 
+
+
+//Stampa i numeri da 1 a 100 
+for( var i = 1 ; i <= 100 ; i++) {
+    
+    //per i numeri multipli di 3 stampa "Fizz"
+    if((i % 3 == 0) && (i % 5 == 0) ) {
+
+        console.log("FizzBuzz");
+        
+    }else if(i % 3 == 0) {
+        
+        console.log("Fizz"); 
+
+    } else if(i % 5 == 0) {
+
+        console.log("Buzz");
+
+    }else {
+
+        console.log(i);
+    }
+ 
+    
+}
+
